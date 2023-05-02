20d0e0c976ca07c139ff6e2f98cc671ddb1bf3e9
Cameron Mikel <CamMikel91@gmail.com>
2023-05-02 08:17:26 -0600

    Added scripts for cart display and local storage.

--- js/shop.js
+++ js/shop.js
@@ -1,5 +1,6 @@
 const cardContainer = document.querySelector("#cardContainer");
 
+// Function to add the products to the shop page.
 function addProducts() {
     products.forEach(individualCard => {
         cardContainer.innerHTML += `
@@ -16,4 +17,48 @@ function addProducts() {
     });
 }
 
+// Function call to populate shop page.
 addProducts();
+
+// Function to save cart info to local storage
+function saveToLocalStorage() {
+    let cartButton = document.getElementsByClassName("cartButton");
+    for (let i = 0; i < cartButton.length; i++) {
+        cartButton[i].addEventListener("click", () => {
+            let selectedProduct = products.find( (product) => product.id == cartButton[i].id);
+            cart.push(selectedProduct);
+            localStorage.setItem("CART", JSON.stringify(cart) );
+            // Cart display refresh on each add to cart click event
+            displayCart();
+        })
+    }
+}
+
+// Function call for saveToLocalStorage().
+saveToLocalStorage();
+
+// Function to populate sideBarContainer with cart items
+function displayCart() {
+    clearStorageAndCart();
+    sideBarContainer.innerHTML = "";
+    cart.forEach(cartItem => {
+        sideBarContainer.innerHTML += `
+        <div class="tableRow">
+            <div class="tableCell">
+                ${cartItem.name}
+            </div>
+            <div class="tableCell">
+                &dollar;${cartItem.price}
+            </div>
+        </div>
+
+        <div class="tableRow">
+            <div class="tableCell borderBottom"></div>
+            <a href="#" id="${cartItem.id}" class="tableCell borderBottom removeLink">Remove</a>
+        </div>
+        `;
+    });
+}
+
+// Cart display for when the page first loads
+displayCart();
