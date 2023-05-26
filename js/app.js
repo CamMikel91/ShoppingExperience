06e38b91139a9084d558a1d63e75cdbfde552b89
Cameron Mikel <CamMikel91@gmail.com>
2023-05-25 21:22:19 -0600

    Added remove/clearCart btn functionality

--- js/app.js
+++ js/app.js
@@ -25,7 +25,12 @@ function removeFromCart() {
             };
             cart.splice(itemIndex, 1);
             localStorage.setItem('CART', JSON.stringify(cart) );
-            displayCart();
+            if (typeof displayCart === "function") {
+                displayCart();
+            }
+            if (typeof displayCartProducts === "function") {
+                displayCartProducts();
+            }
             if (cart.length == 0) {
                 localStorage.clear();
             };
@@ -39,7 +44,12 @@ function clearCart() {
     clearCartButton.addEventListener("click", () => {
         cart = [];
         localStorage.clear();
-        displayCart();
+        if (typeof displayCart === "function") {
+            displayCart();
+        }
+        if (typeof displayCartProducts === "function") {
+            displayCartProducts( );
+            }
     });
 }
 // Function call for clearCart()
